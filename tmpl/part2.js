ecdbf5fef5ea4448caadb24589e7929ca9a81a4d
dariusz.siedlecki <dariusz.siedlecki@fotograf.de>
2023-12-03 13:05:00 +0100

    change file names and add newday.sh to download next day

--- tmpl/part2.js
+++ tmpl/part2.js
@@ -1,3 +1,3 @@
 const fs = require('fs')
-const lines = fs.readFileSync('input.txt', { encoding: 'utf8' }).split('\n')
-
+const lines = fs.readFileSync('example.txt', { encoding: 'utf8' }).split('\n')
+//const lines = fs.readFileSync('input.txt', { encoding: 'utf8' }).split('\n')
