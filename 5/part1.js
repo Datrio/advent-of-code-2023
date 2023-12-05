21a339f6b18bff33fb7a9712e85b5bb97a281d2a
dariusz.siedlecki <dariusz.siedlecki@fotograf.de>
2023-12-05 16:23:47 +0100

    day 5 part 1 in JS done

---
+++ 5/part1.js
@@ -0,0 +1,31 @@
+const fs = require('fs')
+const path = require('path')
+//const lines = fs.readFileSync(path.join(__dirname, 'example.txt'), { encoding: 'utf8' }).trim().split('\n')
+const lines = fs.readFileSync(path.join(__dirname, 'input.txt'), { encoding: 'utf8' }).trim().split('\n')
+
+const seedsRaw = lines.shift().substring(7)
+let seeds = seedsRaw.split(' ').map(Number)
+
+let skipNumbers = {}
+
+lines.forEach(line => {
+    if (line.substring(line.length-4) === 'map:') {
+        // new category
+        skipNumbers = {}
+        return
+    }
+
+    const [destination, source, length] = line.split(' ').map(Number)
+    
+    seeds = seeds.map(seed => {
+        if (seed >= source && seed < source+length && !skipNumbers[seed]) {
+            skipNumbers[seed - source + destination] = true
+
+            return seed - source + destination
+        } else {
+            return seed
+        }
+    })
+})
+
+console.log(Math.min(...seeds))
